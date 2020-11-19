13511bf47536820325c8868ba10a8e9fe6e64036
Eric Roos <ericroos@hey.com>
2020-11-19 16:10:24 -0600

    ok it works

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 //import "bootstrap-3-datepicker/dist/css/bootstrap-datepicker.min.css" rel='stylesheet' type='text/css'>
 import "font-awesome/css/font-awesome.min.css";
 import "animate.css/animate.min.css";
