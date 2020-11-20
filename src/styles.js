a6f4ead5cfe8bdc09da11541ae66cc2c31be9d0c
Eric Roos <ericroos@hey.com>
2020-11-19 21:35:11 -0600

    rework live server

--- src/styles.js
+++ src/styles.js
@@ -3,3 +3,4 @@ import "../vendor/bootstrap-3-datepicker/dist/css/bootstrap-datepicker.min.css";
 import "../vendor/font-awesome/css/font-awesome.min.css";
 import "../vendor/animate.css/animate.min.css";
 import "../vendor/simple-line-icons/css/simple-line-icons.css";
+import "./css/styles-gray.css";
--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,23 @@ module.exports = {
     styles: './src/styles.js'
   },
   output: {
-    filename: '[name].js',
+    filename: 'assets/[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
@@ -38,6 +50,15 @@ module.exports = {
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery"
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
 };
