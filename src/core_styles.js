4fa404e18615ecc214c347a63013e446b2c5f8c0
Eric Roos <ericroos@hey.com>
2020-11-20 16:31:20 -0600

    inline it all

--- src/core_styles.js
+++ src/core_styles.js
@@ -1,1 +1,5 @@
 require("../vendor/bootstrap/dist/css/bootstrap.min.css");
+require("../vendor/bootstrap-3-datepicker/dist/css/bootstrap-datepicker.min.css");
+require("../vendor/font-awesome/css/font-awesome.min.css");
+require("../vendor/animate.css/animate.min.css");
+require("../vendor/simple-line-icons/css/simple-line-icons.css");
--- src/vendor_styles.js
+++
@@ -1,4 +0,0 @@
-require("../vendor/bootstrap-3-datepicker/dist/css/bootstrap-datepicker.min.css");
-require("../vendor/font-awesome/css/font-awesome.min.css");
-require("../vendor/animate.css/animate.min.css");
-require("../vendor/simple-line-icons/css/simple-line-icons.css");
