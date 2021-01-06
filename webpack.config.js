a497852027895463a4929d0d2d6277a5cd2a258c
Eric Roos <ericroos@hey.com>
2021-01-05 21:43:31 -0600

    build title

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const webpack = require('webpack');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const glob = require('glob-all');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CompressionPlugin = require('compression-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HTMLInlineCSSWebpackPlugin = require("html-inline-css-webpack-plugin").default
+const PurgecssPlugin = require('purgecss-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -61,6 +64,14 @@ module.exports = {
       },
     ],
   },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`
+      new CssMinimizerPlugin(),
+    ],
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new ImageMinimizerPlugin({
@@ -84,15 +95,6 @@ module.exports = {
         ],
       },
     }),
-    new MiniCssExtractPlugin({
-      filename: "assets/[name].[contenthash].css",
-      chunkFilename: "assets/[id].[contenthash].css"
-    }),
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
-    }),
-    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       title: 'Custom template',
       template: './src/index.html',
@@ -105,10 +107,26 @@ module.exports = {
     new HTMLInlineCSSWebpackPlugin({
       filter(fileName){
         console.log(fileName);
-        var result = fileName.includes('main') || fileName.includes('index.html');
+        var result = false && (fileName.includes('main') || fileName.includes('index.html'));
         return result;
       }
     }),
+    new MiniCssExtractPlugin({
+      filename: "assets/[name].[contenthash].css",
+      chunkFilename: "assets/[id].[contenthash].css"
+    }),
+    new PurgecssPlugin({
+      paths: glob.sync([
+        './src/**/*',
+        './vendor/**/*'
+      ],
+      { nodir: true })
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new webpack.HotModuleReplacementPlugin(),
     new CompressionPlugin
   ],
   devServer: {
